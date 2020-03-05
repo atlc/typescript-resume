07f5b804955807a7d5a2a35db979ed42bba4f8c0
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-03-05 17:03:17 -0600

    Implementing some more fun with Typescript types, interfaces, and some browser console attributes

--- resume.js
+++ resume.js
@@ -3,6 +3,7 @@ var currentCareer = 'Remote Devops Engineer';
 var desiredCareer = 'Software Engineer, Educator';
 var bio = 'Born on the bayou, in the backseat of a Greyhound bus, Andrew has always been interested in figuring out how things work and tinkering around with them. In early adulthood, discovering programming seemed to be a natural fit for breaking/fixing things, and he was instantly hooked.';
 var interests = ['Programming', 'Linux', 'Microcontrollers', 'Combining the above for Halloween costumes'];
+var logAttrs = 'color: #41FF00; background: black; font-size: 1.3rem; font-family: "Courier New", Courier, monospace';
 var positions = [
     { employer: 'Hubbard Systems', position: 'Devops Engineer', description: 'Automation, CI/CD, Packaging & Distribution' },
     { employer: 'Jefferson State Community College', position: 'Computer Lab Technician', description: 'Kept all labs in operating order. Created automated monitoring to notify of issues immediately. Tutored students in programming classes.' },
@@ -10,6 +11,7 @@ var positions = [
     { employer: 'Greystone Golf & Country Club', position: 'Bartender, Server', description: 'Slinging drinks and all that jazz.' },
 ];
 var skills = [
+    // const skills: ISkill[] = [
     { skillName: 'Electric bass', isCool: true },
     { skillName: 'Electric guitar', isCool: true },
     { skillName: 'Chrome Extension development', isCool: true },
@@ -17,18 +19,19 @@ var skills = [
     { skillName: 'Cheese enthusiast', isCool: false }
 ];
 function displayPositions(place, title, desc) {
-    console.log("* " + title + " @ " + place + "\n    // " + desc);
+    console.log("%c* " + title + " @ " + place + "\n    // " + desc, logAttrs);
 }
 function displaySkills(skill, skillIsCool) {
-    skillIsCool ? console.log("* BAM: " + skill) : console.log("* " + skill);
+    skillIsCool ? console.log("%c* BAM: " + skill, logAttrs) : console.log("%c* " + skill, logAttrs);
 }
-console.warn("Name:\t\t\t" + myName.toUpperCase());
-console.warn("Current occupation:\t" + currentCareer);
-console.warn("Desired occupation:\t" + desiredCareer);
-console.info(bio);
-console.info("\nInterests:");
-interests.forEach(function (interest) { return console.log("* " + interest); });
-console.info("\nExperience:");
+console.warn("%cName:\t\t\t" + myName.toUpperCase(), logAttrs);
+console.warn("%cCurrent occupation:\t" + currentCareer, logAttrs);
+console.warn("%cDesired occupation:\t" + desiredCareer, logAttrs);
+console.info("%c" + bio, logAttrs);
+console.info("%cInterests:", logAttrs);
+interests.forEach(function (interest) { return console.log("%c* " + interest, logAttrs); });
+console.info("%cExperience:", logAttrs);
 positions.forEach(function (job) { return displayPositions(job.employer, job.position, job.description); });
-console.info("\nSkills:");
+console.info("%cSkills:", logAttrs);
 skills.forEach(function (skill) { return displaySkills(skill.skillName, skill.isCool); });
+;
--- resume.ts
+++ resume.ts
@@ -3,15 +3,17 @@ const currentCareer:string = 'Remote Devops Engineer';
 const desiredCareer:string = 'Software Engineer, Educator';
 const bio:string = 'Born on the bayou, in the backseat of a Greyhound bus, Andrew has always been interested in figuring out how things work and tinkering around with them. In early adulthood, discovering programming seemed to be a natural fit for breaking/fixing things, and he was instantly hooked.';
 const interests:Array<string> = ['Programming', 'Linux', 'Microcontrollers', 'Combining the above for Halloween costumes'];
+let logAttrs:string = 'color: #41FF00; background: black; font-size: 1.3rem; font-family: "Courier New", Courier, monospace';
 
-const positions: {employer:string, position:string, description:string}[] = [
+const positions: TPositions[] = [
     { employer: 'Hubbard Systems', position: 'Devops Engineer', description: 'Automation, CI/CD, Packaging & Distribution' },
     { employer: 'Jefferson State Community College', position: 'Computer Lab Technician', description: 'Kept all labs in operating order. Created automated monitoring to notify of issues immediately. Tutored students in programming classes.'},
     { employer: 'Caldwell Mill Animal Clinic', position: 'Kennel Technician', description: 'Took care of everyone\'s critters'},
     { employer: 'Greystone Golf & Country Club', position: 'Bartender, Server', description: 'Slinging drinks and all that jazz.'},
 ];
 
-const skills: {skillName:string, isCool:boolean}[] = [
+const skills: Array<ISkill> = [
+// const skills: ISkill[] = [
     { skillName: 'Electric bass', isCool: true },
     { skillName: 'Electric guitar', isCool: true },
     { skillName: 'Chrome Extension development', isCool: true },
@@ -20,21 +22,23 @@ const skills: {skillName:string, isCool:boolean}[] = [
 ];
 
 function displayPositions(place:string, title:string, desc:string) {
-    console.log(`* ${title} @ ${place}\n    // ${desc}`)
+    console.log(`%c* ${title} @ ${place}\n    // ${desc}`, logAttrs)
 }
 
 function displaySkills(skill:string, skillIsCool:boolean) {
-    skillIsCool ? console.log(`* BAM: ${skill}`) : console.log(`* ${skill}`);
+    skillIsCool ? console.log(`%c* BAM: ${skill}`, logAttrs) : console.log(`%c* ${skill}`, logAttrs);
 }
 
-
-console.warn(`Name:\t\t\t${myName.toUpperCase()}`);
-console.warn(`Current occupation:\t${currentCareer}`);
-console.warn(`Desired occupation:\t${desiredCareer}`);
-console.info(bio);
-console.info(`\nInterests:`);   
-interests.forEach(interest => console.log(`* ${interest}`));
-console.info(`\nExperience:`);
+console.warn(`%cName:\t\t\t${myName.toUpperCase()}`, logAttrs);
+console.warn(`%cCurrent occupation:\t${currentCareer}`, logAttrs);
+console.warn(`%cDesired occupation:\t${desiredCareer}`, logAttrs);
+console.info(`%c${bio}`, logAttrs);
+console.info(`%cInterests:`, logAttrs);   
+interests.forEach(interest => console.log(`%c* ${interest}`, logAttrs));
+console.info(`%cExperience:`, logAttrs);
 positions.forEach(job => displayPositions(job.employer, job.position, job.description));
-console.info(`\nSkills:`);
+console.info(`%cSkills:`, logAttrs);
 skills.forEach(skill => displaySkills(skill.skillName, skill.isCool));
+
+interface ISkill { skillName:string, isCool:boolean };
+type TPositions = { employer:string, position:string, description:string }
